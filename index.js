ec74174fae5c9baa6012be86fafe596389018010
dexter <ritikagrawal0@gmail.com>
2021-06-22 13:46:54 +0530

    cors

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Credentials', true);
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers','X-Requested-With, X-HTTP-Method-Override, Content-Type, enctype, Accept');
+	res.header('Access-Control-Allow-Headers','X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
 	next();
 });
 
